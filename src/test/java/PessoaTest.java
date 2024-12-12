231572eee6020ad9cd26e42ed101b22bfac7df3f
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-12-12 15:44:23 -0300

    finaliza testes dos metodos

--- src/test/java/PessoaTest.java
+++ src/test/java/PessoaTest.java
@@ -66,4 +66,101 @@ class PessoaTest {
         p.setAltura(1F);
         assertEquals(1F,p.getAltura());
     }
+
+    //todos os testes estao no limite de condicao
+    @Test
+    void imcFemininoDeveSerAbaixoPesoMaximo(){ // minimo e o minimo do set
+        Pessoa p = new Pessoa("Feminino",20.6F,1F);
+        assertEquals("abaixo do peso",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerPesoNormalMinimo(){
+        Pessoa p = new Pessoa("Feminino",20.7F,1F);
+        assertEquals("no peso normal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerPesoNormalEmMaximo(){
+        Pessoa p = new Pessoa("Feminino",26.3F,1F);
+        assertEquals("no peso normal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerMarginalmenteEmMinimo(){
+        Pessoa p = new Pessoa("Feminino",26.4F,1F);
+        assertEquals("marginalmente acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerMarginalmenteEmMaximo(){
+        Pessoa p = new Pessoa("Feminino",27.7F,1F);
+        assertEquals("marginalmente acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerAcimaEmMinimo(){
+        Pessoa p = new Pessoa("Feminino",27.8F,1F);
+        assertEquals("acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerAcimaEmMaximo(){
+        Pessoa p = new Pessoa("Feminino",31.0F,1F);
+        assertEquals("acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcFemininoDeveSerObesoEmMinimo(){  //nao existe maximo
+        Pessoa p = new Pessoa("Feminino",31.1F,1F);
+        assertEquals("obeso",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerAbaixoPesoMaximo(){ // minimo e o minimo do set
+        Pessoa p = new Pessoa("Masculino",19.0F,1F);
+        assertEquals("abaixo do peso",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerPesoNormalMinimo(){
+        Pessoa p = new Pessoa("Masculino",19.1F,1F);
+        assertEquals("no peso normal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerPesoNormalEmMaximo(){
+        Pessoa p = new Pessoa("Masculino",25.7F,1F);
+        assertEquals("no peso normal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerMarginalmenteEmMinimo(){
+        Pessoa p = new Pessoa("Masculino",25.8F,1F);
+        assertEquals("marginalmente acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerMarginalmenteEmMaximo(){
+        Pessoa p = new Pessoa("Masculino",27.2F,1F);
+        assertEquals("marginalmente acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerAcimaEmMinimo(){
+        Pessoa p = new Pessoa("Masculino",27.3F,1F);
+        assertEquals("acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerAcimaEmMaximo(){
+        Pessoa p = new Pessoa("Masculino",32.2F,1F);
+        assertEquals("acima do peso ideal",p.calcularImc());
+    }
+
+    @Test
+    void imcMasculinoDeveSerObesoEmMinimo(){  //nao existe maximo
+        Pessoa p = new Pessoa("Masculino",32.3F,1F);
+        assertEquals("obeso",p.calcularImc());
+    }
 }
