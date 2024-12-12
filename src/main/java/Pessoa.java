0873ce0ea885b8b5bb1f38076ec5bb6608d4264a
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-12-12 15:03:51 -0300

    config construtor get e set prontos

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -3,7 +3,40 @@ public class Pessoa {
     private float peso;
     private float altura;
 
-    public String getSexo(){
+    public Pessoa(String sexo, float peso, float altura) {
+        this.setSexo(sexo);
+        this.setPeso(peso);
+        this.setAltura(altura);
+    }
+
+    public String getSexo() {
         return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        if(!(sexo.equals("Masculino") || sexo.equals("Feminino"))) {
+            throw new IllegalArgumentException("Sexo invalido");
+        }
+        this.sexo = sexo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        if (peso<=0)
+            throw new IllegalArgumentException("Peso invalido");
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        if (altura<=0)
+            throw new IllegalArgumentException("Altura invalida");
+        this.altura = altura;
     }
 }
